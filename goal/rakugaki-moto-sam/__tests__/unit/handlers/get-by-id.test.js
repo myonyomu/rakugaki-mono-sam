9cfde71f68ff0d463fc95ace2019ccbf02874f99
myonyomu <myonyomu26@gmail.com>
2022-10-07 01:26:59 +0900

    wip

---
+++ goal/rakugaki-moto-sam/__tests__/unit/handlers/get-by-id.test.js
@@ -0,0 +1,28 @@
+// Import all functions from get-by-id.js 
+const lambda = require('../../../src/handlers/get-by-id.js'); 
+ 
+// This includes all tests for getByIdHandler() 
+describe('Test getByIdHandler', () => { 
+     // This test invokes getByIdHandler() and compare the result  
+    it('should get item by id', async () => { 
+        const item = { id: 'id1' }; 
+        const event = { 
+            httpMethod: 'GET', 
+            pathParameters: { 
+                id: 'id1' 
+            } 
+        } 
+ 
+        // Invoke getByIdHandler() 
+        const result = await lambda.getByIdHandler(event); 
+ 
+        const expectedResult = { 
+            statusCode: 200, 
+            body: JSON.stringify(item) 
+        }; 
+ 
+        // Compare the result with the expected result 
+        expect(result).toEqual(expectedResult); 
+    }); 
+}); 
+ 
---
+++ goal/rakugaki-moto-sam/src/handlers/get-by-id.js
@@ -0,0 +1,26 @@
+/**
+ * A simple example includes a HTTP get method to get one item by id from a DynamoDB table.
+ */
+ exports.getByIdHandler = async (event) => {
+  if (event.httpMethod !== 'GET') {
+    throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
+  }
+  // All log statements are written to CloudWatch
+  console.info('received:', event);
+ 
+  // Get id from pathParameters from APIGateway because of `/{id}` at template.yaml
+  const id = event.pathParameters.id;
+ 
+  // Get the item from the table
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#get-property
+  const item = { id: 'id1' };
+ 
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(item)
+  };
+ 
+  // All log statements are written to CloudWatch
+  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+  return response;
+}
