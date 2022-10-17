a522ac731c6b7cc1dabef8b05d71ce5e37fa6ecc
myonyomu <myonyomu26@gmail.com>
2022-10-17 14:10:21 +0900

    fix tests

--- goal/rakugaki-moto-sam/__tests__/localenv/integration/integration-get-by-id.test.js
+++ goal/rakugaki-moto-sam/__tests__/localenv/integration/integration-get-by-id.test.js
@@ -1,9 +1,20 @@
 const AuthorizationProvider = require("../utils/LocalAuthorizationProvider");
+const got = require("got");
 
 describe("Integration test in local", () => {
   it("can access with cognito dummy", async () => {
     const authProvider = new AuthorizationProvider();
     const auth = await authProvider.generateAuth();
-    
+
+    const accessToken = auth.AuthenticationResult.AccessToken;
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+    const id = 1;
+    const url = `http://127.0.0.1:3000/${id}`;
+    const data = await got(url, { headers, method: "GET" }).json();
+
+    const expected = { id: "id1" };
+    expect(expected).toEqual(data);
   });
 });
--- goal/rakugaki-moto-sam/__tests__/unit/handlers/get-by-id.test.js
+++ goal/rakugaki-moto-sam/__tests__/unit/handlers/get-by-id.test.js
@@ -1,25 +1,16 @@
 // Import all functions from get-by-id.js
 const lambda = require("../../../src/handlers/get-by-id.js");
+const eventJson = require("../../../events/http-api-proxy-event-example.json");
 
 // This includes all tests for getByIdHandler()
 describe("Test getByIdHandler", () => {
   // This test invokes getByIdHandler() and compare the result
   it("should get item by id", async () => {
-    const item = { id: "id1" };
-    const event = {
-      httpMethod: "GET",
-      pathParameters: {
-        id: "id1",
-      },
-    };
-
     // Invoke getByIdHandler()
-    const result = await lambda.getByIdHandler(event);
+    const result = await lambda.getByIdHandler(eventJson);
 
-    const expectedResult = {
-      statusCode: 200,
-      body: JSON.stringify(item),
-    };
+    const item = { id: "id1" };
+    const expectedResult = JSON.stringify(item);
 
     // Compare the result with the expected result
     expect(result).toEqual(expectedResult);
